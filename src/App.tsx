e93193f8bb770487af812e6a4d0ecc0114713297
Brad Fontaine <bradf.83@gmail.com>
2022-05-29 19:11:38 -0600

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { gql, useQuery } from '@apollo/client';
+
+const ALL_PEOPLE = gql`
+  query GetPeople { 
+    allPeople {
+      id
+      name
+      sin
+    }
+  }
+`
+
+const GREETING = gql `
+  query Greeting {
+    greeting
+  }
+`
 
 function App() {
+  const {loading, error, data} = useQuery(ALL_PEOPLE);
+  console.log('loading', loading);
+  console.log('error', error);
+  console.log('data',data);
   return (
     <div className="App">
       <header className="App-header">
